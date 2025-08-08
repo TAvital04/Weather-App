b9d9b50445b6c5694b0b1d32cae2e744faea93cc
Tal Avital <tal.avital04@gmail.com>
2025-08-08 19:51:13 -0400

    test api 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 function App() {
+
+  async function print() {
+    const res = await fetch(
+      "http://api.openweathermap.org/geo/1.0/direct?q={Weston},{FL},{US}&appid={87260a6c3d87520ec68da8d6e238d658}"
+    );
+
+    const data = await res.json();
+    return data;
+  }
+
   return (
     <>
-      <h1>App</h1>
+      {print()}
     </>
   )
 }
