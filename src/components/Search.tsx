1cb45cb77a02d96d96c6cb625265d8ba17f024e5
Tal Avital <tal.avital04@gmail.com>
2025-08-09 12:17:57 -0400

    error messages

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -10,20 +10,24 @@ function Search() {
     const [lng, setLng] = useState("")
     const [coords, setCoords] = useState<Coord | null>(null)
 
-    function tryCoords(latData: string, lngData: string) {
+    const [error, setError] = useState(false)
+
+    function validateCoords(latData: string, lngData: string) {
         let lat = Number(latData)
         let lng = Number(lngData)
 
         let latBool = false
         let lngBool = false
 
-        if(!Number.isNaN(lat)) {
+        console.log(`>>${latData === ""}<< >>${lngData === ""}<<`)
+
+        if(!Number.isNaN(lat) && latData != "") {
             if(lat >= -90 && lat <= 90) {
                 latBool = true
             }
         }
 
-        if(!Number.isNaN(lng)) {
+        if(!Number.isNaN(lng) && lngData != "") {
             if(lng >= -180 && lng <= 180) {
                 lngBool = true
             }
@@ -31,33 +35,54 @@ function Search() {
 
         if(latBool && lngBool) {
             setCoords({lat, lng})
+            setError(false)
+        } else {
+            setError(true)    
         }
+
+        if(!error) setError(true)
     }
 
     return (
-        <>
-            <h1>Search</h1>
+        <div className = "contents">
+            <h1 className = "title">Search</h1>
 
-            <input
-                type = "number"
-                placeholder = "latitude"
-                value = {lat}
-                onChange = {(e) => setLat(e.target.value)}
-            />
+            <div className = "search">
+                <div className = "input">
+                    <input
+                        type = "number"
+                        placeholder = "latitude"
+                        value = {lat}
+                        onChange = {(e) => setLat(e.target.value)}
+                    />
 
-            <input
-                type = "number"
-                placeholder = "longitude"
-                value = {lng}
-                onChange = {(e) => setLng(e.target.value)}
-            />
+                    <input
+                        type = "number"
+                        placeholder = "longitude"
+                        value = {lng}
+                        onChange = {(e) => setLng(e.target.value)}
+                    />
+                </div>
 
-            <button
-                onClick = {() => tryCoords(lat, lng)}
-            >Submit</button>
+                <div className = "error">
+                    {error && 
+                        <ul>
+                            <li>Latitude must be greater than -90 and less than 90</li>
+                            <li>Longitude must be greater than -180 and less than 180</li>
+                        </ul>
+                    }
+                </div>
+
+                <div className = "submit">
+                    <button
+                        onClick = {() => validateCoords(lat, lng)}
+                    >Submit</button>        
+                </div>
+            </div>
+
 
             {coords != null && <h1>{`${coords.lat}, ${coords.lng}`}</h1>}
-        </>
+        </div>
     )
 }
 
