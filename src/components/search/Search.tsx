8179f29bcbbd70c875d94ab5cb17d7713703f590
Tal Avital <tal.avital04@gmail.com>
2025-08-12 12:19:55 -0400

    format search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -3,20 +3,26 @@ import {useState} from "react"
 import SearchCoords from "./SearchCoords.tsx"
 import SearchZip from "./SearchZip.tsx"
 
+import style from "../../styles/search.module.css"
+
 const Search = (props: any) => {
     const [coords, setCoords] = useState(true)
     return (
         <>
-            <h1>Search</h1>
-            <button onClick={() => setCoords(!coords)}>
-                {coords? "Search By Zip": "Search By Coordinates"}
-            </button>
-
-            {coords && 
-                coords? 
-                    <SearchCoords setApiData = {props.setApiData}/>: 
-                    <SearchZip setApiData = {props.setApiData}/>
-            }
+            <div className = {style.contents}>
+                <h1>Search</h1>
+                
+                <div className = {style.search}>
+                    <button onClick={() => setCoords(!coords)}>
+                        {coords? "Search By Zip": "Search By Coordinates"}
+                    </button>
+                    
+                    {coords?
+                        <SearchCoords setApiData = {props.setApiData}/>: 
+                        <SearchZip setApiData = {props.setApiData}/>
+                    }
+                </div>
+            </div>
         </>
     )
 }
