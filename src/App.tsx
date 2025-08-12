8ba664b98daeea3f9335c9bebf97cd7f6c5dc665
Tal Avital <tal.avital04@gmail.com>
2025-08-12 10:49:26 -0400

    app contents border

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import {useState} from "react"
 import Search from "./components/search/Search.tsx"
 import Weather from "./components/weather/Weather.tsx"
 
+import style from "./styles/app.module.css"
+
 const App = () => {
   const [apiData, setApiData] = useState(null)
 
   return (
-    <>
+    <div className = {style.contents}>
       <Search setApiData = {setApiData}/>
       {apiData && <Weather apiData = {apiData}/>}
-    </>
+    </div>
   )
 }
 
