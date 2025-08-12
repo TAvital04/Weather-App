55604112c2a5c1a10496c547a8a5765655af99e2
Tal Avital <tal.avital04@gmail.com>
2025-08-12 18:36:35 -0400

    render date and time

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 import Search from "./components/search/Search.tsx"
 import Weather from "./components/weather/Weather.tsx"
 
 import style from "./styles/app.module.css"
 
+const key = import.meta.env.VITE_API_KEY
+
 const App = () => {
   const [apiData, setApiData] = useState(null)
+
+  useEffect(() => {
+    const reRenderData = async (apiData: any) => {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${apiData.lat}&lon=${apiData.lon}&units=metric&appid=${key}`
+      )
+
+      const resJSON = await res.json()
+
+      setApiData(resJSON)
+    }
+
+    const interval = setInterval(() => {
+      if(apiData) {
+        reRenderData(apiData)
+      }
+    }, 60 * 1000)
+
+    return () => clearInterval(interval)
+  }, [apiData])
 
   return (
     <div className = {style.contents}>
--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -5,9 +5,21 @@ import WeatherHourly from "./WeatherHourly.tsx"
 import WeatherDaily from "./WeatherDaily.tsx"
 
 const key = import.meta.env.VITE_API_KEY
+const days = [
+    "Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"
+]
+const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+
+import style from "../../styles/weather.module.css"
 
 const Weather = (props: any) => {
     const [location, setLocation] = useState("")
+    const [date, setDate] = useState("")
+    const [time, setTime] = useState("")
 
     useEffect(() => {
         const fetchData = async (apiData: any) => {
@@ -15,20 +27,45 @@ const Weather = (props: any) => {
                 `https://api.openweathermap.org/geo/1.0/reverse?lat=${apiData.lat}&lon=${apiData.lon}&limit=1&appid=${key}`
             )
 
-            const resJSON = await res.json()
+            if(res.ok) {
+                const resJSON = await res.json()
 
-            setLocation(`${resJSON[0].name}, ${resJSON[0].country}`)
+                setLocation(`${resJSON[0].name}, ${resJSON[0].country}`)
+            }
         }
 
         fetchData(props.apiData)
+
+    }, [])
+
+    useEffect(() => {
+        const newDate = new Date(props.apiData.current.dt * 1000)
+        setDate(`
+            ${days[newDate.getDay()]}, 
+            ${months[newDate.getMonth()]} 
+            ${newDate.getDay()}
+        `)
+        setTime(`
+            ${newDate.getHours() % 12}:
+            ${newDate.getMinutes()}:
+            ${newDate.getSeconds()}
+            ${newDate.getHours() > 12? "PM": "AM"}
+        `)
     }, [props.apiData])
 
     return (
         <div className = "contents">
             {location != "" && <h2>{location}</h2>}
-            <WeatherCurrent apiData = {props.apiData}/>
-            <WeatherHourly apiData = {props.apiData}/>
-            <WeatherDaily apiData = {props.apiData}/>
+
+            <div className = {style.body}>
+                {props.apiData &&
+                    <h4>{date}: {time}</h4> ||
+
+                    <WeatherCurrent apiData = {props.apiData}/> ||
+                    <WeatherHourly apiData = {props.apiData}/> || 
+                    <WeatherDaily apiData = {props.apiData}/>
+                }
+            </div>
         </div>
     )
 }
